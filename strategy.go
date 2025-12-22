3e2de50d1a75ce24537a751446538e9e09fa32f8
alimohammad <alimohammad1995@gmail.com>
2025-12-22 16:38:48 +0100

    oo

--- strategy.go
+++ strategy.go
@@ -1,21 +1,54 @@
 package main
 
+const Workers = 5
+
 type Strategy struct {
 	client   *PolymarketClient
 	executor *OrderExecutor
+	markets  chan string
 }
 
 func NewStrategy(client *PolymarketClient) *Strategy {
-	return &Strategy{
+	strategy := &Strategy{
 		client:   client,
 		executor: NewOrderExecutor(client),
+		markets:  make(chan string, Workers),
 	}
+	strategy.Run()
+	return strategy
 }
 
 func (s *Strategy) OnOrderBookUpdate(assetID []string) {
+	marketsToCheck := map[string]bool{}
+	for _, id := range assetID {
+		if marketID, ok := TokenToMarketID[id]; ok {
+			marketsToCheck[marketID] = true
+			s.markets <- marketID
+		}
+	}
 
 }
 
 func (s *Strategy) OnAssetUpdate(assetID []string) {
+	marketsToCheck := map[string]bool{}
+	for _, id := range assetID {
+		if marketID, ok := TokenToMarketID[id]; ok {
+			marketsToCheck[marketID] = true
+			s.markets <- marketID
+		}
+	}
+}
 
+func (s *Strategy) Run() {
+	for i := 0; i < Workers; i++ {
+		go func() {
+			for {
+				market := <-s.markets
+				s.handle(market)
+			}
+		}()
+	}
+}
+
+func (s *Strategy) handle(marketID string) {
 }
